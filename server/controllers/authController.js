3df30a3a0d7dea636cac7a1c0adfeaec3c043969
bokadepranav <89243706+bokadepranav@users.noreply.github.com>
2025-05-11 14:10:21 +0530

    created backend using mvc model

---
+++ server/controllers/authController.js
@@ -0,0 +1,43 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+exports.register = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ username });
+    if (!existingUser) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+    if (isMatch) {
+      return res.status(200).json({ message: "Login successful" });
+    } else {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -2,8 +2,7 @@
 
 const mongoose = require('mongoose');
 const express = require('express');
-const User = require('./user')
-
+const authRoutes = require('./routes/authRoutes');
 // mongoDb connection
 
 const mongoURI = 'mongodb+srv://pranavbokade279:Pranav%24279@cluster0.tnukm2q.mongodb.net/shopworm?retryWrites=true&w=majority&appName=Cluster0';
@@ -23,26 +22,7 @@ mongoose.connect(mongoURI, {
 const app = express();
 app.use(express.json()); // middleware to parse res.body data
 
-app.post("/register", async (req, res) => {
-  const {username, password} = req.body;
-  
-  try {
-    const existingUser = await User.findOne({ username });
-    if(existingUser)
-    {
-      return res.status(400).json({message: "User already exists"});
-    }
-    
-    const newUser = new User({username, password});
-    await newUser.save();
-
-    res.status(201).json({message: "User registered successfully"});
-  }
-  catch(err) {
-    console.log(err);
-    res.status(500).json({error: "Server error"});
-  }
-});
+app.use('/auth', authRoutes);
 
 app.listen(5000, () => {
   console.log('Server is running on http://localhost:5000');
