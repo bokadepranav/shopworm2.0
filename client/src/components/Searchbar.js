439ce9dda10c43692cc0f6d55756ba6fc712d5e0
bokadepranav <89243706+bokadepranav@users.noreply.github.com>
2025-05-16 23:26:56 +0530

    generated navbar

--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function Searchbar() {
   return (
-    <div className='search-bar-wrapper'>
-      <input type="text" className='search-bar-input' placeholder='Search for products, categories, brand, etc.'/>
-      <img src="" alt="..." className='search-bar-icon' />
+    <div className="search-bar-wrapper">
+      <input
+        type="text"
+        className="search-bar-input"
+        placeholder="Search for products, categories, brand, etc."
+      />
+      <button className="search-bar-btn">
+        <i className="fa-solid fa-magnifying-glass search-bar-icon" />
+      </button>
     </div>
-  )
+  );
 }
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -7,6 +7,17 @@ export default function Navbar() {
       <div className="navbar-wrapper">
         <span className="navbar-logo">Shopworm.</span>
         <Searchbar />
+        <div className="navbar-login-wrapper">
+          <button className="navbar-login-user-btn"><i className="fa-solid fa-user navbar-login-user-btn-icon"></i></button>
+          <div className="navbar-login-user-wrapper">
+            <span className="navbar-login-user-greetings">Hello,</span>
+            <span className="navbar-login-user-name">SignUp</span>
+          </div>
+        </div>
+        <div className="navbar-menu-btn-wrapper">
+          <button className="navbar-menu-btn"><i class="fa-solid fa-heart navbar-menu-btn-icon"></i></button>
+          <button className="navbar-menu-btn"><i class="fa-solid fa-cart-shopping navbar-menu-btn-icon"></i></button>
+        </div>
       </div>
     </section>
   );
