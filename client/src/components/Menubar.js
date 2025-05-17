4f95f9f22b8a51fae07428c6d0b959f1aa2fb390
bokadepranav <89243706+bokadepranav@users.noreply.github.com>
2025-05-17 23:57:08 +0530

    updated menubar

--- client/src/components/Menubar.js
+++ client/src/components/Menubar.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import {Link} from "react-router-dom";
 
 export default function Menubar() {
+
+  const [activePage, setActivePage] = useState(document.getElementById("HOME"));
+
+  const handleLinkClick = (currPage) => {
+    activePage.classList.remove("active");
+
+    setActivePage(document.getElementById(currPage));
+    activePage.classList.add("active");
+  }
+
   return (
     <div className="menubar-container">
       <div className="menubar-item-wrapper">
-        <a href="/" className="menubar-item-link">
-          <span className="menubar-item">HOME</span>
-        </a>
-        <a href="/" className="menubar-item-link">
-          <span className="menubar-item">SHOP</span>
-        </a>
-        <a href="/" className="menubar-item-link">
-          <span className="menubar-item">BLOG</span>
-        </a>
-        <a href="/" className="menubar-item-link">
-          <span className="menubar-item">ABOUT</span>
-        </a>
-        <a href="/" className="menubar-item-link">
-          <span className="menubar-item">CONTACT</span>
-        </a>
+        <Link to="/" className="menubar-item-link">
+          <span className="menubar-item active" onClick={handleLinkClick} id="HOME">HOME</span>
+        </Link>
+        <Link to="/" className="menubar-item-link">
+          <span className="menubar-item" onClick={handleLinkClick} id="SHOP">SHOP</span>
+        </Link>
+        <Link to="/" className="menubar-item-link">
+          <span className="menubar-item" onClick={handleLinkClick} id="BLOG">BLOG</span>
+        </Link>
+        <Link to="/" className="menubar-item-link">
+          <span className="menubar-item" onClick={handleLinkClick} id="ABOUT">ABOUT</span>
+        </Link>
+        <Link to="/" className="menubar-item-link">
+          <span className="menubar-item" onClick={handleLinkClick} id="CONTACT">CONTACT</span>
+        </Link>
       </div>
     </div>
   );
