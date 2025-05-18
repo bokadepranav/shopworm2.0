0cb0d5973ebee5f86835a7e7abfb7bc1c0d69ea3
bokadepranav <89243706+bokadepranav@users.noreply.github.com>
2025-05-18 17:14:30 +0530

    created category banner for home

---
+++ client/src/components/CategoryBanner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function CategoryBanner(props) {
+    console.log(props.backgroundImage)
+  return (
+    <div className='category-banner-wrapper background' style={{backgroundImage: props.backgroundImage}}> 
+      <span className="text-font heading-6 bold color-blue mb-2">{props.categoryText}</span>
+      <span className="text-font heading-4 bold color-black mb-2">{`Min ${props.categoryDiscount}% Off`}</span>
+      <span className="text-small text-font color-black mb-3">{props.categoryDescription}</span>
+      <button className="btn btn-primary heading-6 bold text-font">SHOP NOW</button>
+    </div>
+  )
+}
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import heroBg from '../assets/catalog/catalog-slider-1.jpg';
-import buttonBg from '../assets/button.png'
+import React from "react";
+import heroBg from "../assets/catalog/catalog-slider-1.jpg";
+import buttonBg from "../assets/button.png";
+import CategoryBanner from "../components/CategoryBanner";
+
+import categoryBanner1 from '../assets/catalog/catalog-banner-1.jpg';
+import categoryBanner2 from '../assets/catalog/catalog-banner-2.jpg';
+import categoryBanner3 from '../assets/catalog/catalog-banner-3.jpg';
 
 export default function Home() {
   return (
     <>
-      <div className="hero-banner-wrapper" style={{backgroundImage: `url(${heroBg})`}}>
+      {/* hero banner */}
+      <div
+        className="hero-banner-wrapper"
+        style={{ backgroundImage: `url(${heroBg})` }}
+      >
         <span className="heading-4 bold mb-3 color-black">Festive-feast</span>
-        <span className="color-black text-font hero-banner-heading">Super value deals</span>
-        <span className="mb-3 color-blue text-font hero-banner-heading">On all products</span>
-        <span className="heading-5 color-grey text-font mb-3">Save more with coupons & upto 70% off!</span>
-        <button className="hero-banner-btn heading-5 bold color-blue" style={{backgroundImage: `url(${buttonBg})`}}>Shop now</button>
+        <span className="color-black text-font hero-banner-heading">
+          Super value deals
+        </span>
+        <span className="mb-3 color-blue text-font hero-banner-heading">
+          On all products
+        </span>
+        <span className="heading-5 color-grey text-font mb-3">
+          Save more with coupons & upto 70% off!
+        </span>
+        <button
+          className="hero-banner-btn heading-5 bold color-blue"
+          style={{ backgroundImage: `url(${buttonBg})` }}
+        >
+          Shop now
+        </button>
+      </div>
+
+      {/* category banners */}
+      <div className="home-category-banner-wrapper">
+        <CategoryBanner backgroundImage={`url(${categoryBanner1})`} categoryText={"WHITE SNEAKERS"} categoryDiscount={30} categoryDescription={"Men Fashionable Shoes"}/>
+        <CategoryBanner backgroundImage={`url(${categoryBanner2})`} categoryText={"WOMEN'S FASHION"} categoryDiscount={65} categoryDescription={"Shoes & Backpacks"}/>
+        <CategoryBanner backgroundImage={`url(${categoryBanner3})`} categoryText={"TRENDY GLASSES"} categoryDiscount={50} categoryDescription={"Fashionable Glasses"}/>
       </div>
     </>
-  )
+  );
 }
