f134cc2c3feb552658fcf755855c050d36c7121e
bokadepranav <89243706+bokadepranav@users.noreply.github.com>
2025-05-23 19:26:36 +0530

    modified med banner

--- client/src/components/Banner/MediumBanner.js
+++ client/src/components/Banner/MediumBanner.js
@@ -4,9 +4,10 @@ import bannerImage from '../../assets/banner/b17.jpg'
 export default function MediumBanner() {
   return (
     <div className='medium-banner-wrapper' style={{backgroundImage: `url(${bannerImage})`}}>
-      <span className="text-font heading-6 color-white">crazy deals</span>
-      <span className="text-font heading-5 bold color-white">buy 1 get 1 free</span>
-      <span className="text text-font color-white">The best classic dress is on sale at cara</span>
+      <span className="text-font heading-4 color-white my-1">crazy deals</span>
+      <span className="text-font heading-2 bolder color-white my-1">buy 1 get 1 free</span>
+      <span className="heading-6 text-font color-white my-1">The best classic dress is on sale at cara</span>
+      <button className="btn border-color-white background-color-transparent color-white my-2 text-font text bold">Learn More</button>
     </div>
   )
 }
