52e246e893ea546c291c96a8584a36609df4d355
bokadepranav <89243706+bokadepranav@users.noreply.github.com>
2025-05-25 10:59:41 +0530

    modified news letter

--- client/src/components/Banner/NewsLetter.js
+++ client/src/components/Banner/NewsLetter.js
@@ -1,15 +1,32 @@
-import React from 'react'
-import backgroundImage from '../../assets/banner/b14.png'
+import React from "react";
+import backgroundImage from "../../assets/banner/b14.png";
 
 export default function NewsLetter() {
   return (
-    <div className='news-letter-wrapper' style={{backgroundImage: `url(${backgroundImage})`}}>
+    <div
+      className="news-letter-wrapper"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
       <div className="row">
-        <div className="col-md-7">
-            <span className="text-font heading-3 bold color-white">Sign Up for New Letters</span>
+        <div className="col-md-7 news-letter-left-container">
+          <span className="text-font heading-3 bold color-white my-1">
+            Sign Up for New Letters
+          </span>
+          <span className="text-font heading-6 bold color-light-grey my-1">
+            Get email updates for our latest shop and{" "}
+            <span className="text-font heading-6 color-yellow">
+              special offers.
+            </span>
+          </span>
         </div>
-        <div className="col-md-5"></div>
+        <div className="col-md-5 news-letter-right-container">
+          <input
+            type="text"
+            placeholder="Your Email Address"
+            className="text-font heading-6 color-grey news-letter-input"
+          />
+        </div>
       </div>
     </div>
-  )
+  );
 }
