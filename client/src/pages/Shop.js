7789b83175ec223a8924ea07b3d7a307d555db43
bokadepranav <89243706+bokadepranav@users.noreply.github.com>
2025-05-28 22:59:52 +0530

    completed shop page

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import PageHeadBanner from '../components/Banner/PageHeadBanner'
+import HeadingDescription from '../components/HeadingDescription'
+import ProductGroup from '../components/ProductGroup'
+import NewsLetter from '../components/Banner/NewsLetter'
 
-export default function Shop() {
+export default function Shop(props) {
   return (
     <div>
+      {/* page header banner */}
       <PageHeadBanner headerTitle="#stayhome" headerSubTitle="Save more with coupons & upto 70% off!" />
+    
+      {/* shop product */}
+      <HeadingDescription heading="New Arrivals" description="Get upto 70% off on all fashion brands!" />
+
+      <ProductGroup products={props.data.products} length={16} />
+    
+      {/* news letter */}
+      <NewsLetter />
     </div>
   )
 }
