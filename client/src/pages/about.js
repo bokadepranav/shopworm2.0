2da5fb61eb17d0aff353ba1a7f612f43386d5bc2
bokadepranav <89243706+bokadepranav@users.noreply.github.com>
2025-05-29 21:23:06 +0530

    started about page

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import PageHeadBanner from '../components/Banner/PageHeadBanner'
+import backgroundImage from '../assets/about/banner.png'
 
 export default function About() {
   return (
     <div>
-      about
+      <PageHeadBanner backgroundImage={backgroundImage} headerTitle="#knowUs" headerSubTitle="wanna know who we are?" />
     </div>
   )
 }
--- client/src/pages/blogs.js
+++ client/src/pages/blogs.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PageHeadBanner from '../components/Banner/PageHeadBanner'
 import NewsLetter from '../components/Banner/NewsLetter'
+import backgroundImage from '../assets/banner/b2.jpg'
 
 export default function Blogs() {
   return (
     <div>
 
       {/* page header banner */}
-      <PageHeadBanner backgroundImage="../assets/banner/b2.jpg" headerTitle="#readmore" headerSubTitle="Read all case studies about our products!" />
+      <PageHeadBanner backgroundImage={backgroundImage} headerTitle="#readmore" headerSubTitle="Read all case studies about our products!" />
 
       {/* news letter */}
       <NewsLetter />
